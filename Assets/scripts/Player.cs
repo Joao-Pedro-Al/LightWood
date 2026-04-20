d7ea0b7223fa442a340610c1ce59de689ee279a0
Joao-Pedro-Al <pv27644@alunos.estgv.ipv.pt>
2026-04-20 10:10:52 +0100

    20-04 - Movimento do Personagem e Câmara

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -5,17 +5,29 @@ public class Player : MonoBehaviour
     // Créditos: https://discussions.unity.com/t/first-person-movement/677313
     #region "Variables"
     public Rigidbody Rigid;
-    public float MouseSensitivity;
+    public float MouseSensitivity_Horizontal;
+    public float MouseSensitivity_Vertical;
     public float MoveSpeed;
     public float JumpForce;
+    public Transform camera;
     #endregion
-   
+
+    private float verticalRotation = 0f;
+
     void Update ()
     {
-        Rigid.MoveRotation(Rigid.rotation * Quaternion.Euler(new Vector3(0, Input.GetAxis("Mouse X") * MouseSensitivity, 0)));
-        Rigid.MoveRotation(Rigid.rotation * Quaternion.Euler(new Vector3(0, Input.GetAxis("Mouse Y") * MouseSensitivity, 0)));
+        // Camera-Horizontal
+        Rigid.MoveRotation(Rigid.rotation * Quaternion.Euler(new Vector3(0, Input.GetAxis("Mouse X") * MouseSensitivity_Horizontal, 0)));
+
+        // Camera-Vertical
+        float mouseY = Input.GetAxis("Mouse Y") * MouseSensitivity_Vertical;
+        verticalRotation -= mouseY;
+        verticalRotation = Mathf.Clamp(verticalRotation, -90f, 90f);
+        camera.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+
+        // Movimento
         Rigid.MovePosition(transform.position + (transform.forward * Input.GetAxis("Vertical") * MoveSpeed) + (transform.right * Input.GetAxis("Horizontal") * MoveSpeed));
-        if (Input.GetKeyDown("space"))
-            Rigid.AddForce(transform.up * JumpForce);
+        // if (Input.GetKeyDown("space"))
+        //     Rigid.AddForce(transform.up * JumpForce);
     }
 }
