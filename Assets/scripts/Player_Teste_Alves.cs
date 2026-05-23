bad8ed17d7aae47bb4af311ba29b8e520938141f
Joao-Pedro-Al <pv27644@alunos.estgv.ipv.pt>
2026-05-23 18:20:12 +0100

    23-05 - Fazer Sprint ao clicar no Left Shift

---
+++ Assets/scripts/Player_Teste_Alves.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class Player_Teste_Alves : MonoBehaviour
+{
+    // Créditos: https://discussions.unity.com/t/first-person-movement/677313
+    #region "Variables"
+    public Rigidbody Rigid;
+    public float MouseSensitivity_Horizontal;
+    public float MouseSensitivity_Vertical;
+    public float MoveSpeed;
+    public float RunSpeed;
+    public float JumpForce;
+    public Transform camera;
+    #endregion
+
+    private float verticalRotation = 0f;
+
+    void Update ()
+    {
+        // Camera-Horizontal
+        Rigid.MoveRotation(Rigid.rotation * Quaternion.Euler(new Vector3(0, Input.GetAxis("Mouse X") * MouseSensitivity_Horizontal, 0)));
+
+        // Camera-Vertical
+        float mouseY = Input.GetAxis("Mouse Y") * MouseSensitivity_Vertical;
+        verticalRotation -= mouseY;
+        verticalRotation = Mathf.Clamp(verticalRotation, -90f, 90f);
+        camera.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+
+        // Movimento
+        float VelocidadeAtual = MoveSpeed;
+        if(Input.GetKey("left shift"))
+        {
+            VelocidadeAtual = RunSpeed;
+        }
+
+        // Debug.Log(VelocidadeAtual);
+
+        Rigid.MovePosition(transform.position + (transform.forward * Input.GetAxis("Vertical") * VelocidadeAtual) + (transform.right * Input.GetAxis("Horizontal") * VelocidadeAtual));
+        // if (Input.GetKeyDown("space"))
+        //     Rigid.AddForce(transform.up * JumpForce);
+    }
+}
