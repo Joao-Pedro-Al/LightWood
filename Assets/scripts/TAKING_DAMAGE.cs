c0902307a77d8071ab4dd2953f5b7276f17f8b41
demonicartist <madalena.iaf05@gmail.com>
2026-06-05 14:10:12 +0100

    m

---
+++ Assets/scripts/TAKING_DAMAGE.cs
@@ -0,0 +1,31 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TAKING_DAMAGE : MonoBehaviour
+{
+    public int health = 1;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("DAMAGE"))
+        {
+            TakeDamage(1);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
